bb9a859aca449774eaefc092dcb0c36fbdb90e33
Harshvardhan Mishra <harshvardhanmishra13@gmail.com>
2025-04-29 12:48:11 +0530

    Updated ui

--- Backend/database.js
+++ Backend/database.js
@@ -6,7 +6,7 @@ class Database {
     if (!Database.instance) {
       this.pool = mysql.createPool({
         host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT),
+        // port: parseInt(process.env.DB_PORT),
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
--- Frontend/Utilities/constant.js
+++ Frontend/Utilities/constant.js
@@ -1,2 +1,2 @@
-export const API_BASEURL="http://localhost:8080/api"
-// export const API_BASEURL = "http://56.228.6.122:8080/api";
+//export const API_BASEURL="http://localhost:8080/api"
+export const API_BASEURL = "http://56.228.6.122:8080/api";
